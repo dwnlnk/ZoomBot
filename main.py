64df265b2e5030ebb35a099893ce5ba8e79b47c8
Jordan Reger <jordanreger@gmail.com>
2020-10-29 13:32:44 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-import os, schedule, time, pyautogui
+import tkinter as tk
+import time, webbrowser, sys
 
-for i in range(3):
-    os.chdir("..")
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.labelUrl = tk.Label(text="URL")
+        self.entryUrl = tk.Entry(bd=3)
+        self.labelTime = tk.Label(text="Time (24H)")
+        self.entryTime = tk.Entry(bd=3)
+        self.root.geometry("370x420")
+        self.root.title('ZOOMBot')
+        self.root.iconbitmap('logo.ico')
+        self.labelUrl.pack()
+        self.entryUrl.pack()
+        self.labelTime.pack()
+        self.entryTime.pack()
+        self.submit = tk.Button(text="run", command=self.openZoom)
+        self.submit.pack()
+        self.updateTime()
+        self.root.mainloop()
 
-def openZoom():
-    os.chdir("AppData/Roaming/Zoom/bin") #enter the path of zoom.exe
-    os.system("zoom.exe")
-    pyautogui.moveTo(780, 780)
-    pyautogui.click()
-    pyautogui.moveTo(850, 500)
-    time.sleep(0.5)
-    pyautogui.typewrite('*********') #enter the call "code"
-    pyautogui.moveTo(950, 700)
-    pyautogui.click()
-    time.sleep(1)
-    pyautogui.moveTo(1000, 800)
-    pyautogui.click()
-    time.sleep(3)
-    pyautogui.keyDown('altleft')
-    pyautogui.press('h')
-    pyautogui.keyUp('altleft')
-    time.sleep(0.5)
-    pyautogui.moveTo(1800, 900)
-    pyautogui.typewrite("(Enter Name Here)'s ZoomBot: Good day! (Enter name here) will be here shortly. Thank you for your patience!") #enter your name
-    time.sleep(1)
-    pyautogui.press('enter')
+    def updateTime(self):
+        self.now = time.strftime("%H:%M")
+        self.root.after(1000, self.updateTime)
 
-schedule.every().day.at("08:00").do(openZoom) #enter what time you want it to start
+    def openUrl(self):
+        url = self.entryUrl.get()
+        webbrowser.open_new_tab(url)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    def openZoom(self):
+        self.startTime = self.entryTime.get()
+        startHour = self.startTime[1]
+        if startHour == ":":
+            self.startTime = str("0" + startTime)
+        if(self.now == self.startTime):
+            self.openUrl()
+            sys.exit()
+        self.root.after(1000, self.openZoom)
+
+app=App()
